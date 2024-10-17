e54ba22e982fc361a5fec78527583e49966245f8
Pâris <paris.gougne.05@gmail.com>
2024-10-17 11:06:28 +0200

    feature : issue 1

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -220,3 +220,85 @@ export default function Profile() {
         </div>
     );
 }
+
+/*<form onSubmit={faire fonction} className="flex flex-col gap-5">
+    <input className=""
+type="text"
+value={name}
+onChange={(e) => setName(e.target.value)}
+placeholder="Name"
+required
+/>
+<input
+type="text"
+value={surname}
+onChange={(e) => setSurname(e.target.value)}
+placeholder="Surname"
+required
+/>
+<input
+type="email"
+value={email}
+onChange={(e) => setEmail(e.target.value)}
+placeholder="Email"
+required
+/>
+<input
+type="password"
+value={password}
+onChange={(e) => setPassword(e.target.value)}
+placeholder="Password"
+required
+/>
+<input
+type="date"
+value={dateOfBirth}
+onChange={(e) => setDateOfBirth(e.target.value)}
+placeholder="Date of Birth"
+required
+/>
+<select
+value={gender}
+onChange={(e) => setGender(e.target.value)}
+required
+>
+<option>Homme</option>
+<option>Femme</option>
+<option>Autre</option>
+</select>
+
+<select
+    value={sexualOrientation}
+    onChange={(e) => setSexualOrientation(e.target.value)}
+    required
+>
+    <option>Hétérosexuel</option>
+    <option>Homosexuel</option>
+    <option>Bisexuel</option>
+    <option>Autre</option>
+</select>
+<input
+    type="text"
+    value={profilePicture}
+    onChange={(e) => setProfilPicture(e.target.value)}
+    placeholder="Profil picture"
+    required
+/>
+<input
+    type="text"
+    value={bio}
+    onChange={(e) => setBio(e.target.value)}
+    placeholder="Bio"
+    required
+/>
+<input
+    type="text"
+    value={location}
+    onChange={(e) => setLocation(e.target.value)}
+    placeholder="Location"
+    required
+/>
+
+<button type="submit">Login</button>
+</form>
+*/
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,23 +1,51 @@
 import { useState } from 'react';
+import bcrypt from "bcryptjs";
 
 export default function Signup() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
     const [surname, setSurname] = useState('');
-    const [dateOfBirth, setDateOfBirth] = useState('');
-    const [gender, setGender] = useState('');
-    const [sexualOrientation, setSexualOrientation] = useState('');
-    const [profilePicture, setProfilPicture] = useState('');
-    const [bio, setBio] = useState('');
-    const [location, setLocation] = useState('');
-    const [createdAt, setCreatedAt] = useState('');
-    const [updateAt, setUpdateAt] = useState('');
+    const [date_of_birth, setDateOfBirth] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const hashedPassword = await bcrypt.hash(password, 10)
+        // Collect form data
+        const formData = {
+            name,
+            surname,
+            email,
+            password: hashedPassword,
+            date_of_birth,
+        };
+
+        // Send data to backend
+        try {
+            const response = await fetch('http://localhost:3001/api/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            const result = await response.json();
+            if (response.ok) {
+                alert('User registered successfully');
+            } else {
+                alert('Error: ' + result.error);
+            }
+        } catch (error) {
+            console.error('Error during user signup:', error);
+        }
+    };
+
 
     return (
         <div>
-            <h1>Login</h1>
-            <form /*onSubmit={faire fonction}*/ className="flex flex-col gap-5">
+            <h1>Sign up</h1>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-5">
                 <input className=""
                     type="text"
                     value={name}
@@ -48,54 +76,12 @@ export default function Signup() {
                 />
                 <input
                     type="date"
-                    value={dateOfBirth}
+                    value={date_of_birth}
                     onChange={(e) => setDateOfBirth(e.target.value)}
                     placeholder="Date of Birth"
                     required
                 />
-                <select
-                    value={gender}
-                    onChange={(e) => setGender(e.target.value)}
-                    required
-                >
-                    <option>Homme</option>
-                    <option>Femme</option>
-                    <option>Autre</option>
-                </select>
-
-                <select
-                    value={sexualOrientation}
-                    onChange={(e) => setSexualOrientation(e.target.value)}
-                    required
-                >
-                    <option>Hétérosexuel</option>
-                    <option>Homosexuel</option>
-                    <option>Bisexuel</option>
-                    <option>Autre</option>
-                </select>
-                <input
-                    type="text"
-                    value={profilePicture}
-                    onChange={(e) => setProfilPicture(e.target.value)}
-                    placeholder="Profil picture"
-                    required
-                />
-                <input
-                    type="text"
-                    value={bio}
-                    onChange={(e) => setBio(e.target.value)}
-                    placeholder="Bio"
-                    required
-                />
-                <input
-                    type="text"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="Location"
-                    required
-                />
-
-                <button type="submit">Login</button>
+                <button type="submit">Sign Up</button>
             </form>
         </div>
     );
