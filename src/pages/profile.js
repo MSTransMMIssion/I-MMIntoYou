2411e8fa57db23b4c4c2a5425cff403cc2dd8dcf
CallMeTrinity <anteur.ap@gmail.com>
2024-10-24 09:49:49 +0200

    feature : signup improvements

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import axios from "axios";
+import axios from 'axios';
 import { useRouter } from 'next/router';
 
 export default function Profile() {
@@ -16,7 +16,6 @@ export default function Profile() {
             setUser(userData);
             setFormData(userData);
         } else {
-            // Si l'utilisateur n'est pas connecté, rediriger vers la page de login
             router.push('/login');
         }
     }, [router]);
@@ -44,10 +43,9 @@ export default function Profile() {
         }
 
         try {
-            await axios.put(`/api/users/update/${user.id}`, formData); // Mettre à jour les données utilisateur
-            setUser(formData); // Mettre à jour les données dans l'état
-            setIsEditing(false); // Quitter le mode édition
-            // Mettre à jour les informations dans localStorage
+            await axios.put(`/api/users/update/${user.id}`, formData);
+            setUser(formData);
+            setIsEditing(false);
             localStorage.setItem('loggedUser', JSON.stringify(formData));
         } catch (error) {
             console.error('Erreur lors de la mise à jour des données utilisateur', error);
@@ -63,131 +61,125 @@ export default function Profile() {
     };
 
     return (
-        <div className="max-w-4xl mx-auto p-6 bg-gray-100">
-            <div className="bg-white rounded-xl shadow-xl p-8 flex items-center space-x-6">
-                <img
-                    src={user.profile_picture || '/placeholder-avatar.png'}
-                    alt="User Avatar"
-                    className="w-24 h-24 rounded-full object-cover border-2 border-blue-500"
-                />
-                {!isEditing ? (
+        <div className="max-w-5xl mx-auto p-6 bg-gradient-to-br from-blue-50 to-purple-50 min-h-screen flex items-center justify-center">
+            <div className="bg-white rounded-3xl shadow-2xl p-10 w-full">
+                {/* Header du profil */}
+                <div className="flex items-center space-x-8 mb-10">
+                    <img
+                        src={user.profile_picture || '/placeholder-avatar.png'}
+                        alt="User Avatar"
+                        className="w-28 h-28 rounded-full object-cover border-4 border-blue-500 shadow-md"
+                    />
                     <div>
-                        <h1 className="text-4xl font-extrabold text-gray-900">{user.name} {user.surname}</h1>
-                        <p className="text-lg text-gray-500 mt-1">{user.bio}</p>
+                        {!isEditing ? (
+                            <>
+                                <h1 className="text-5xl font-bold text-gray-900">{user.name} {user.surname}</h1>
+                                <p className="text-lg text-gray-500 mt-2">{user.bio}</p>
+                            </>
+                        ) : (
+                            <form onSubmit={handleSubmit} className="space-y-6 w-full">
+                                <div className="flex space-x-4">
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        value={formData.name}
+                                        onChange={handleChange}
+                                        placeholder="Nom"
+                                        className="w-full p-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition"
+                                    />
+                                    {errors.name && <p className="text-red-500">{errors.name}</p>}
+                                    <input
+                                        type="text"
+                                        name="surname"
+                                        value={formData.surname}
+                                        onChange={handleChange}
+                                        placeholder="Prénom"
+                                        className="w-full p-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition"
+                                    />
+                                    {errors.surname && <p className="text-red-500">{errors.surname}</p>}
+                                </div>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    placeholder="Email"
+                                    className="w-full p-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition"
+                                />
+                                {errors.email && <p className="text-red-500">{errors.email}</p>}
+                                <input
+                                    type="date"
+                                    name="date_of_birth"
+                                    value={formData.date_of_birth}
+                                    onChange={handleChange}
+                                    className="w-full p-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition"
+                                />
+                                <select
+                                    name="gender"
+                                    value={formData.gender}
+                                    onChange={handleChange}
+                                    className="w-full p-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition"
+                                >
+                                    <option value="male">Homme</option>
+                                    <option value="female">Femme</option>
+                                    <option value="other">Autre</option>
+                                </select>
+                                <select
+                                    className="w-full p-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition"
+                                    name="sexual_orientation"
+                                    value={formData.sexual_orientation}
+                                    onChange={handleChange}
+                                >
+                                    <option value="heterosexual">Hétérosexuel</option>
+                                    <option value="gay">Homosexuel</option>
+                                    <option value="bisexual">Bisexuel</option>
+                                </select>
+                                <textarea
+                                    name="bio"
+                                    value={formData.bio}
+                                    onChange={handleChange}
+                                    placeholder="Parlez-nous de vous..."
+                                    className="w-full p-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition"
+                                />
+                                <input
+                                    type="text"
+                                    name="location"
+                                    value={formData.location}
+                                    onChange={handleChange}
+                                    placeholder="Localisation"
+                                    className="w-full p-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition"
+                                />
+                            </form>
+                        )}
                     </div>
-                ) : (
-                    <form onSubmit={handleSubmit} className="space-y-4">
-                        <div>
-                            <input
-                                type="text"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                className="text-4xl font-extrabold text-gray-900 p-2 border rounded-lg w-full"
-                            />
-                            {errors.name && <p className="text-red-500">{errors.name}</p>}
-                        </div>
-                        <div>
-                            <input
-                                type="text"
-                                name="surname"
-                                value={formData.surname}
-                                onChange={handleChange}
-                                className="text-4xl font-extrabold text-gray-900 p-2 border rounded-lg w-full"
-                            />
-                            {errors.surname && <p className="text-red-500">{errors.surname}</p>}
-                        </div>
-                        <div>
-                            <input
-                                type="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                className="p-2 border rounded-lg w-full"
-                                placeholder="Email"
-                            />
-                            {errors.email && <p className="text-red-500">{errors.email}</p>}
-                        </div>
-                        <div>
-                            <input
-                                type="date"
-                                name="date_of_birth"
-                                value={formData.date_of_birth}
-                                onChange={handleChange}
-                                className="p-2 border rounded-lg w-full"
-                            />
-                        </div>
-                        <div>
-                            <select
-                                name="gender"
-                                value={formData.gender}
-                                onChange={handleChange}
-                                className="p-2 border rounded-lg w-full"
-                            >
-                                <option value="male">Homme</option>
-                                <option value="female">Femme</option>
-                                <option value="other">Autre</option>
-                            </select>
-                        </div>
-                        <div>
-                            <select
-                                className="p-2 border rounded-lg w-full"
-                                name="sexual_orientation"
-                                value={formData.sexual_orientation}
-                                onChange={handleChange}
-                            >
-                                <option value="heterosexual">Hétérosexuel</option>
-                                <option value="gay">Homosexuel</option>
-                                <option value="bisexual">Bisexuel</option>
-                            </select>
-                        </div>
-                        <div>
-                            <textarea
-                                name="bio"
-                                value={formData.bio}
-                                onChange={handleChange}
-                                className="p-2 border rounded-lg w-full"
-                                placeholder="Bio"
-                            />
-                        </div>
-                        <div>
-                            <input
-                                type="text"
-                                name="location"
-                                value={formData.location}
-                                onChange={handleChange}
-                                className="p-2 border rounded-lg w-full"
-                                placeholder="Location"
-                            />
-                        </div>
-                    </form>
-                )}
-            </div>
+                </div>
 
-            <div className="mt-8 text-center">
-                {!isEditing ? (
-                    <button
-                        className="bg-blue-500 text-white font-semibold px-8 py-3 rounded-lg shadow-lg hover:bg-blue-600 transition duration-300"
-                        onClick={() => setIsEditing(true)}
-                    >
-                        Modifier le profil
-                    </button>
-                ) : (
-                    <div className="flex justify-center space-x-4">
+                {/* Boutons */}
+                <div className="text-center mt-8">
+                    {!isEditing ? (
                         <button
-                            onClick={handleSubmit}
-                            className="bg-green-500 text-white font-semibold px-8 py-3 rounded-lg shadow-lg hover:bg-green-600 transition duration-300"
+                            className="bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold px-8 py-3 rounded-full shadow-lg hover:opacity-90 transition-transform transform hover:scale-105"
+                            onClick={() => setIsEditing(true)}
                         >
-                            Sauvegarder
+                            Modifier le profil
                         </button>
-                        <button
-                            onClick={() => setIsEditing(false)}
-                            className="bg-red-500 text-white font-semibold px-8 py-3 rounded-lg shadow-lg hover:bg-red-600 transition duration-300"
-                        >
-                            Annuler
-                        </button>
-                    </div>
-                )}
+                    ) : (
+                        <div className="flex justify-center space-x-4">
+                            <button
+                                onClick={handleSubmit}
+                                className="bg-green-500 text-white font-semibold px-8 py-3 rounded-full shadow-lg hover:bg-green-600 transition-transform transform hover:scale-105"
+                            >
+                                Sauvegarder
+                            </button>
+                            <button
+                                onClick={() => setIsEditing(false)}
+                                className="bg-red-500 text-white font-semibold px-8 py-3 rounded-full shadow-lg hover:bg-red-600 transition-transform transform hover:scale-105"
+                            >
+                                Annuler
+                            </button>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
