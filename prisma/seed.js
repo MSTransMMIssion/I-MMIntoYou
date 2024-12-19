f3246f67a2d460d288f929fc91ada943af7b6bed
CallMeTrinity <anteur.ap@gmail.com>
2024-12-19 15:24:54 +0100

    fix : good pictures for genders

--- prisma/seed.js
+++ prisma/seed.js
@@ -18,6 +18,9 @@ async function main() {
         const birthdate = generateBirthdate(18, 30);
         const password = await bcrypt.hash('chef', 10);
 
+        // Générer le genre
+        const gender = faker.helpers.arrayElement(['male', 'female']);
+
         // Création d'un utilisateur
         const user = await prisma.user.create({
             data: {
@@ -26,26 +29,29 @@ async function main() {
                 email: faker.internet.email(),
                 password: password,
                 date_of_birth: birthdate,
-                gender: faker.helpers.arrayElement(['male', 'female']),
+                gender: gender,
                 sexual_orientation: faker.helpers.arrayElement(['heterosexual', 'bisexual', 'homosexual']),
                 bio: faker.lorem.sentence(),
                 location: "Grenoble",
                 createdAt: new Date(),
                 updatedAt: new Date(),
-                min_age_preference: faker.number.int({min: 18, max: 24}),
-                max_age_preference: faker.number.int({min: 25, max: 35})
+                min_age_preference: faker.number.int({ min: 18, max: 24 }),
+                max_age_preference: faker.number.int({ min: 25, max: 35 })
             },
         });
+
+        // Générer des photos de profil en fonction du genre
+        const genderPath = gender === 'female' ? 'women' : 'men';
         for (let j = 0; j < 5; j++) {
             const profilePicture = await prisma.profilePicture.create({
                 data: {
                     userId: user.id,
-                    url: `https://randomuser.me/api/portraits/women/${i+j}.jpg`,
+                    url: `https://randomuser.me/api/portraits/${genderPath}/${i + j}.jpg`,
                 }
             });
         }
 
-        console.log(`Utilisateur ${user.name} créé avec succès avec des préférences.`);
+        console.log(`Utilisateur ${user.name} (${gender}) créé avec succès avec des préférences.`);
     }
 }
 
