0405bb03ea4162b19db72451028cf490a670eefd
II-SARIP-II <paris.gougne.05@gmail.com>
2024-12-19 02:32:36 +0100

    feat : cannot send a message to a undefined user

--- src/pages/messages/[id].js
+++ src/pages/messages/[id].js
@@ -2,6 +2,7 @@ import Conversation from '@/components/Conversation';
 import { useRouter } from 'next/router';
 import {useEffect, useState} from "react";
 import CryptoJS from "crypto-js";
+import axios from "axios";
 
 //decode
 
@@ -20,8 +21,27 @@ export default function Id() {
 
         const bytes = CryptoJS.AES.decrypt(decodeURIComponent(chaineDeCaracteres), secretKey);
         console.log(bytes.toString(CryptoJS.enc.Utf8));
-        setOtherUserId(parseInt(bytes.toString(CryptoJS.enc.Utf8)));
+        let otherUser = parseInt(bytes.toString(CryptoJS.enc.Utf8));
+        verifyOtherUserExist(otherUser)
     }, []);
+
+    const verifyOtherUserExist = async (otherUser) => {
+        try {
+            const response = await axios.get("/api/users"); // Assurez-vous que cette route retourne les utilisateurs
+            const users = response.data.data;
+            console.log(users);
+            const userExists = users.find(user => user.id === otherUser);
+
+            if (userExists) {
+                console.log("L'utilisateur est réel");
+            } else {
+                console.log("L'utilisateur n'est pas réel")
+                router.push('/messages')
+            }
+        } catch (error) {
+            console.error("Erreur lors de la récupération des utilisateurs:", error);
+        }
+    }
 
     useEffect(() => {
          setUserId(JSON.parse(localStorage.getItem('loggedUser')).id);
